c1b113279597a65b9ae6802d4cf458b03db79f2d
Wiktor <w.meyer-1@student.utwente.nl>
2024-12-07 18:09:37 +0100

    Initial commit

---
+++ src/games/guessNumber/GuessNumber.java
@@ -0,0 +1,62 @@
+package games.guessNumber;
+
+import java.util.Optional;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+/**
+ * A simple guess number game
+ * You input the number to the console until you
+ * guess the number that was generated randomly
+ * By default the number are in [1,10] range
+ */
+public class GuessNumber {
+
+    private static final int MIN = 1;
+    private static final int MAX = 10;
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int randomNumber = generateRandomNumber(MIN, MAX);
+        System.out.println("Guess the number between 1 and 10:");
+        int enteredNumber = readNumber();
+        while (enteredNumber != randomNumber) {
+            System.out.println("Incorrect :c Try again!");
+            enteredNumber = readNumber();
+        }
+        System.out.println("Correct! The number was: " + randomNumber);
+    }
+
+    /**
+     * reads the number
+     * @return an integer that is in range
+     */
+    private static int readNumber() {
+        Optional<Integer> enteredNumber = Optional.empty();
+        while (enteredNumber.isEmpty()) {
+            try {
+                int input = parseInt(SCANNER.nextLine());
+                if (input < MIN || input > MAX) {
+                    System.out.println("That is outside the range.");
+                    System.out.printf("Please enter a number between %d and %d.%n", MIN, MAX);
+                }else{
+                    enteredNumber = Optional.of(input);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number.");
+            }
+        }
+        return enteredNumber.get();
+    }
+
+    /**
+     *
+     * @param min lower bound of the interval
+     * @param max upper bound of the interval
+     * @return an integer between min and max inclusive
+     */
+    private static int generateRandomNumber(int min, int max) {
+        return (int) (Math.random() * (max - min + 1) + min);
+    }
+}
---
+++ src/games/guessNumber/Slots.java
@@ -0,0 +1,126 @@
+package games.guessNumber;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class Slots {
+    public static int balance = 100;
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws InterruptedException {
+         boolean play = true;
+
+        System.out.println("************************");
+        System.out.println("Welcome to Java Slots!");
+        System.out.println("Symbols: 🍒 🍉 🍋 🔔 ⭐");
+        System.out.println("************************\n");
+
+        while (play) {
+            int bet = 0;
+            int winnings = 0;
+            if (balance > 0){
+                System.out.println("Current Balance: " + balance +"$");
+                System.out.println("Enter bet amount: ");
+                bet = inputBet();
+                balance -= bet;
+                System.out.println("Spinning...");
+                TimeUnit.SECONDS.sleep(1);
+                winnings= determineWinnings(spinRow(),bet);
+                TimeUnit.MILLISECONDS.sleep(500);
+                System.out.println("You won "+ winnings+"$");
+                balance += winnings;
+                System.out.println("Do you want to play again? (Y/N)");
+                play = playAgain();
+            }else{
+                System.out.println("You lost all you money. :(");
+                play = false;
+            }
+        }
+        System.out.println("Thank you for playing!");
+    }
+
+    /**
+     * @return A String array of length 3 with random symbols
+     */
+    public static String[] spinRow(){
+        String[] row = new String[3];
+        String[] symbols = {"🍒", "🍉", "🍋", "🔔", "⭐"};
+        Random rand = new Random();
+
+        for (int i = 0; i < 3; i++) {
+            row[i] = symbols[rand.nextInt(symbols.length)];
+        }
+        System.out.println(Arrays.toString(row));
+        return row;
+    }
+
+    /**
+     * A methods to get the bet amount from the user
+     * @return amount that the user wants to bet
+     */
+    public static int inputBet(){
+        Optional<Integer> bet = Optional.empty();
+
+        while (bet.isEmpty()) {
+            try{
+                bet = Optional.of(Integer.parseInt(scanner.nextLine()));
+                if (bet.get() > balance){
+                    System.out.println("You don't have enough money!");
+                    bet = Optional.empty();
+                }else if(bet.get() <= 0){
+                    System.out.println("Bet must be greater than 0");
+                    bet = Optional.empty();
+                    System.out.println("Enter bet amount: ");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Enter an integer");
+                System.out.println("Enter bet amount: ");
+            }
+        }
+        return bet.get();
+    }
+
+    /**
+     * A method to determine the amount won
+     * @param row Already spun row of symbols
+     * @param bet Amount bet by the player
+     * @return Amount of $ won by the player
+     */
+    public static int determineWinnings(String[] row, int bet){
+        if (row[0].equals(row[1]) && row[1].equals(row[2])){
+            return switch (row[1]) {
+                case ("🍒") -> bet * 3;
+                case ("🍉") -> bet * 4;
+                case ("🍋") -> bet * 5;
+                case ("🔔") -> bet * 10;
+                case ("⭐") -> bet * 20;
+                default -> 0;
+            };
+        }
+        if (row[0].equals(row[1]) || row[1].equals(row[2])){
+            return switch (row[1]) {
+                case ("🍒") -> bet * 2;
+                case ("🍉") -> bet * 3;
+                case ("🍋") -> bet * 4;
+                case ("🔔") -> bet * 5;
+                case ("⭐") -> bet * 10;
+                default -> 0;
+            };
+        }
+        return 0;
+    }
+
+    /**
+     * Assert if the player want to play again
+     * @return true/false
+     */
+    public static boolean playAgain(){
+        while (true) {
+            try {
+                return scanner.nextLine().equalsIgnoreCase("y");
+            } catch (Exception e) {
+                System.out.println("Enter y or n");
+            }
+        }
+    }
+}
