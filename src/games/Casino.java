f38553538232f81c4ebc9d606510f690d8f39ada
Wiktor <w.meyer-1@student.utwente.nl>
2024-12-08 19:00:00 +0100

    implemented inputBalance() method and added balance and scanner variables to the Casino clas

--- src/games/Casino.java
+++ src/games/Casino.java
@@ -1,5 +1,36 @@
 package games;
 
+import java.util.Optional;
+import java.util.Scanner;
+
 public class Casino {
+    private int balance;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Casino!");
+        System.out.println("What is your balance?");
+    }
+
+    public static int inputBalance() {
+        Optional<Integer> balance = Optional.empty();
+        while (!balance.isPresent()) {
+            try{
+                int input = Integer.parseInt(scanner.nextLine());
+                if (input > 0){
+                    balance = Optional.of(input);
+                }else{
+                    System.out.println("Your balance has to be greater than 0");
+                }
+            }catch(NumberFormatException e){
+                System.out.println("Please enter a number!");
+            }
+        }
+    }
+
 
 }
