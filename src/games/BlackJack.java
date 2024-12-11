2a1630e23e75a7d04f700b6c9ab91600cc98be96
Wiktor <w.meyer-1@student.utwente.nl>
2024-12-11 20:21:52 +0100

    Started implementing Blackjack- added playBlackJack() and its constructor. Added classes Card and CardDeck

--- src/games/BlackJack.java
+++ src/games/BlackJack.java
@@ -1,5 +1,18 @@
 package games;
 
-public class BlackJack {
+public class BlackJack extends Game {
+
+    public BlackJack(Casino casino) {
+        setCasino(casino);
+    }
+
+    public void playBlackJack() {
+        boolean play = true;
+        System.out.println("************************");
+        System.out.println("Welcome to Java BlackJack!");
+        System.out.println("************************\n");
+
+
+    }
 
 }
