170c900d7a99f6d008d7a8938cf5b8c696be7c43
Wiktor <w.meyer-1@student.utwente.nl>
2024-12-17 19:47:32 +0100

    Added variables to Card, so that suits and rank have a defined text

--- src/games/model/Card.java
+++ src/games/model/Card.java
@@ -4,6 +4,28 @@ public class Card {
     private final String rank;
     private final String suit;
 
+    // ---- constants -----------------------------------
+
+    // ranks are 2, ..., 9 and:
+    public static final String JACK = "Jack";
+    public static final String QUEEN = "Queen";
+    public static final String KING = "King";
+    public static final String ACE = "Ace";
+    public static final String TEN = "Ten";
+
+    // suits are:
+    public static final String CLUBS = "Clubs";
+    public static final String DIAMONDS = "Diamonds";
+    public static final String HEARTS = "Hearts";
+    public static final String SPADES = "Spades";
+
+    // some convenient arrays
+    public static final String[] RANK_ARRAY = {"2", "3", "4", "5", "6", "7",
+            "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+    public static final String[] SUIT_ARRAY = {"Clubs", "Diamonds",
+            "Hearts", "Spades"};
+
+
     public Card(String rank, String suit) {
         this.rank = rank;
         this.suit = suit;
@@ -19,6 +41,6 @@ public class Card {
 
     @Override
     public String toString() {
-        return rank + "of" + suit;
+        return rank + " of " + suit;
     }
 }
