c1dc9546a3d4bc1c1e512333ee9dea1607f0f76c
Wiktor <w.meyer-1@student.utwente.nl>
2024-12-17 19:47:50 +0100

    Implementation of the class CardDeck

--- src/games/model/CardDeck.java
+++ src/games/model/CardDeck.java
@@ -1,4 +1,34 @@
 package games.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static games.model.Card.*;
+
+
 public class CardDeck {
+    private ArrayList<Card> cards;
+
+    public CardDeck() {
+        cards = new ArrayList<>();
+    }
+
+    public void addCards(){
+        for (String suit : SUIT_ARRAY) {
+            for (String rank : RANK_ARRAY) {
+                Card card = new Card(rank, suit);
+                cards.add(card);
+            }
+        }
+    }
+
+    public void shuffleDeck() {
+        Collections.shuffle(cards);
+    }
+
+    public static void main(String[] args) {
+        CardDeck deck = new CardDeck();
+        deck.shuffleDeck();
+        System.out.println(deck.cards);
+    }
 }
